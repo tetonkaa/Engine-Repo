7559f60ebebaa04987c800fe5fefc265c1ffcf5b
Angel <angel.l.puente@gmail.com>
2022-11-21 09:37:35 -0600

    added to server.js

--- server.js
+++ server.js
@@ -6,14 +6,27 @@ const { default: mongoose } = require("mongoose");
 const PassengerVehicle = require('./models/passengerVehicle');
 const RecreationalVehicle = require('./models/recreationalVehicle.js');
 
-
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 
 
 // Middleware
-// Body parser middleware: give us access to req.body
+// add ejs view engine, add POST requests, add access to req.body
 app.use(express.static('style'));
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+
+//Routes
+//Routes for all applicable controllers.
+app.use('/', passengerCtrl)
 
 
+//Listener
+//Binds and listens to connections on specified host and port
+app.listen(port, () => {
+    console.log(`App is running at localhost:${port}`)
+})
